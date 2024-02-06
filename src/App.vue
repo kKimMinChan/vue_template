9e9ac20ca1c9ad45150942c1a3f76270f103e63d
김민찬 <kimminchan@gimminchan-ui-MacBookAir.local>
2024-02-06 16:03:07 +0900

    vue template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "~/components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    return {
+      message: "Hello Vue!!!",
+    };
+  },
+};
+</script>
